747f1f2fff1c234f2c9a9d0aba937c0f5c01595c
ehkim <keh7264@gmail.com>
2018-12-12 13:22:23 +0900

    setting up Firebase as a Dummy Backend

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,9 +1,21 @@
-import { Component } from '@angular/core';
+import { DataStorageService } from "./../shared/data-storage.service";
+import { Component } from "@angular/core";
+import { Response } from "@angular/http";
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html'
+  selector: "app-header",
+  templateUrl: "./header.component.html"
 })
 export class HeaderComponent {
+  constructor(private dataStorageSerivce: DataStorageService) {}
 
+  onSaveData() {
+    this.dataStorageSerivce.storeRecipes().subscribe((response: Response) => {
+      console.log(response);
+    });
+  }
+
+  onFetchData() {
+    this.dataStorageSerivce.getRecipes();
+  }
 }
