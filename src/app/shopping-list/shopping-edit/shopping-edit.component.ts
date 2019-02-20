d38f341cf2537758c1ebf3341ca91b30d39304a4
ehkim <keh7264@gmail.com>
2019-02-20 22:30:42 +0900

    Setting up the auth store files

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,26 +1,25 @@
-import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
-import { NgForm } from '@angular/forms';
-import { Store } from '@ngrx/store';
-import { Subscription } from 'rxjs';
-import { Ingredient } from '../../shared/ingredient.model';
-import * as ShoppingListActions from '../store/shopping-list.actions';
-import * as fromShoppingList from '../store/shopping-list.reducers';
-
+import { Component, OnDestroy, OnInit, ViewChild } from "@angular/core";
+import { NgForm } from "@angular/forms";
+import { Store } from "@ngrx/store";
+import { Subscription } from "rxjs";
+import { Ingredient } from "../../shared/ingredient.model";
+import * as fromApp from "../../store/app.reducers";
+import * as ShoppingListActions from "../store/shopping-list.actions";
 @Component({
-  selector: 'app-shopping-edit',
-  templateUrl: './shopping-edit.component.html',
-  styleUrls: ['./shopping-edit.component.css']
+  selector: "app-shopping-edit",
+  templateUrl: "./shopping-edit.component.html",
+  styleUrls: ["./shopping-edit.component.css"]
 })
 export class ShoppingEditComponent implements OnInit, OnDestroy {
-  @ViewChild('f') slForm: NgForm;
+  @ViewChild("f") slForm: NgForm;
   subscription: Subscription;
   editMode = false;
   editItem: Ingredient;
 
-  constructor(private store: Store<fromShoppingList.AppState>) { }
+  constructor(private store: Store<fromApp.AppState>) {}
 
   ngOnInit() {
-    this.subscription = this.store.select('shoppingList').subscribe(data => {
+    this.subscription = this.store.select("shoppingList").subscribe(data => {
       if (data.editedIngredientIndex > -1) {
         this.editItem = data.editedIngredient;
         this.editMode = true;
@@ -43,9 +42,11 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
     const value = form.value;
     const newIngredient = new Ingredient(value.name, value.amount);
     if (this.editMode) {
-      this.store.dispatch(new ShoppingListActions.UpdateIngredient({ ingredient: newIngredient }));
+      this.store.dispatch(
+        new ShoppingListActions.UpdateIngredient({ ingredient: newIngredient })
+      );
     } else {
-      this.store.dispatch(new ShoppingListActions.AddIngredient(newIngredient))
+      this.store.dispatch(new ShoppingListActions.AddIngredient(newIngredient));
     }
     this.editMode = false;
     form.reset();
@@ -57,7 +58,6 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
   }
 
   onDelete() {
-
     this.store.dispatch(new ShoppingListActions.DeleteIngredient());
     this.onClear();
   }
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,24 +1,22 @@
-import { Component, OnInit } from '@angular/core';
-import { Store } from '@ngrx/store';
-import { Observable } from 'rxjs';
-import * as ShoppingListActions from './store/shopping-list.actions';
-import * as fromShoppingList from './store/shopping-list.reducers';
-
-
+import { Component, OnInit } from "@angular/core";
+import { Store } from "@ngrx/store";
+import { Observable } from "rxjs";
+import * as fromApp from "../store/app.reducers";
+import * as ShoppingListActions from "./store/shopping-list.actions";
+import * as fromShoppingList from "./store/shopping-list.reducers";
 
 @Component({
-  selector: 'app-shopping-list',
-  templateUrl: './shopping-list.component.html',
-  styleUrls: ['./shopping-list.component.css']
+  selector: "app-shopping-list",
+  templateUrl: "./shopping-list.component.html",
+  styleUrls: ["./shopping-list.component.css"]
 })
 export class ShoppingListComponent implements OnInit {
   shoppingListState: Observable<fromShoppingList.State>;
 
-  constructor(
-    private store: Store<fromShoppingList.AppState>) { }
+  constructor(private store: Store<fromApp.AppState>) {}
 
   ngOnInit() {
-    this.shoppingListState = this.store.select('shoppingList');
+    this.shoppingListState = this.store.select("shoppingList");
   }
 
   onEditItem(index: number) {
